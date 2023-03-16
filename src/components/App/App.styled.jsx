859f643e1fe711b582bb117ace6438676f0a349a
Roman <sayga76239@gmail.com>
2023-03-16 13:44:23 +0100

    final

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -16,13 +16,13 @@ export const Section = styled.section`
 export const H1 = styled.h1`
   color:#333;
   margin-bottom: 15px;
-  font-size: 24px;
+  font-size: 30px;
   font-weight: bold;
 `;
 
 export const H2 = styled.h2`
   color:#333;
   margin-bottom: 15px;
-  font-size: 20px;
+  font-size: 30px;
   font-weight: bold;
 `;
